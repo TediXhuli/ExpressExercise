ede80da3888174758891b10bcacc0bfd8c1397d2
Tedi Xhuli <xhuli.tedi@gmail.com>
2022-06-16 17:05:31 +0200

    added docker to the project

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const Server = () => {
   const App = express();
   App.use(bodyParser.json());
 
+  App.get("/", (req, res) => {
+    res.json({
+      message: "Erik Rocks",
+    });
+  });
+
   App.use(Router);
 
-  App.listen(3000, "127.0.0.1", () => {
+  App.listen(3000, () => {
     console.log("Server started");
   });
 };
